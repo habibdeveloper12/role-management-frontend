4d4dee45e59300ac3533dd8687c76a0182972792
habibur rahman <habibur.rahman@elementzerolabs.com>
2023-12-14 01:39:15 +0600

    table isse

--- src/Pages/Table/Table.jsx
+++ src/Pages/Table/Table.jsx
@@ -101,12 +101,11 @@ const Table = () => {
                       Ip Address
                     </th>
                   )}
-                  {role == ADMIN ||
-                    (role == SUPER_USER && (
-                      <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                        Email
-                      </th>
-                    ))}
+                  {(role == ADMIN || role == SUPER_USER) && (
+                    <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                      Email
+                    </th>
+                  )}
 
                   <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
                     friends
